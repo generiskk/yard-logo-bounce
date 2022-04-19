c0c5f162309e579d14b5e460a5a2727eb89f4560
generiskk <36735685+generiskk@users.noreply.github.com>
2022-04-20 00:57:53 +0200

    Update main.js
    gotta round that shit down

--- inverted/js/main.js
+++ inverted/js/main.js
@@ -5,8 +5,8 @@ let ctx;
 let logoColor;
 
 let dvd = {
-    x: Math.random() * (window.innerWidth - 544),
-    y: Math.random() * (window.innerHeight - 528),
+    x: Math.round(Math.random() * (window.innerWidth - 544)),
+    y: Math.round(Math.random() * (window.innerHeight - 528)),
     xspeed: 3,
     yspeed: 3,
     img: new Image()
